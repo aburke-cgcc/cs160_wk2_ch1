5c7f53d3e379f113104b3c50ac3dfd1f033b3b5b
aburke-cgcc <55819868+aburke-cgcc@users.noreply.github.com>
2019-10-01 16:14:34 -0700

    HelloWorld Upload

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		char[] str = new char[] {'H','e','l','l','o',' ', 'W','o','r','l','d'};
+		System.out.println("Using System.out.println: Hello World!");
+		
+		System.out.print("Using TestClass.printString: ");
+		TestClass.printString(str);
+	}
+
+}
---
+++ HelloWorld/src/TestClass.java
@@ -0,0 +1,7 @@
+public class TestClass {
+	public static void printString(char[] str) {
+		for(int i = 0; i < str.length; i++) {
+			System.out.print(str[i]);
+		}
+	}
+}
